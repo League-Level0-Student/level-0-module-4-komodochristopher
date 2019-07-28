945e48ba616b130dcc0b22ca372b2d2decf433fd
League Student <league@iMac-27.attlocal.net>
2019-07-27 17:31:07 -0700

    pi and strings

--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,46 +1,61 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday , isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
- int stick;
+		boolean isWeekday=false;
+		boolean isVacation=false;
+
+		/* You MUST use the above boolean variables in your code */
+
+		int stick;
 
 // int choice = ;
- 
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-   int regular = JOptionPane.showConfirmDialog(null, "Is it a weekday? ", "Sleepy head" ,JOptionPane.YES_NO_OPTION);
-if(regular == 1  ) {
-isWeekday = false;
-}
-if(regular == 0) {
-isWeekday = true;
-}
- System.out.println(regular);
- int vacation = JOptionPane.showConfirmDialog(null,"are you on vacation?");
- if(vacation ==1) {
-isVacation = false;
- }
- 
- 
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         
-         * print “get up lazy bones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
+
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int regular = JOptionPane.showConfirmDialog(null, "Is it a weekday? ", "Sleepy head",
+				JOptionPane.YES_NO_OPTION);
+		if (regular == 1) {
+			isWeekday = false;
+		}
+		if (regular == 0) {
+			isWeekday = true;
+		}
+		System.out.println(regular);
+
+		int Vacation = JOptionPane.showConfirmDialog(null, "are you on vacation?","Sleepy head",JOptionPane.YES_NO_OPTION);
+		if (Vacation == 1) {
+
+			isVacation = false;
+
+		}
+		if(Vacation == 0) {
+		isVacation = true;	
+		}
+
+		/*
+		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
+		 * 
+		 * print “get up lazy bones!�? If it is a weekday, and we are on vacation, print
+		 * “sleep in�?.
+		*/
+		if(isVacation || !isWeekday ||(isWeekday && isVacation)) {
+			JOptionPane.showMessageDialog(null, "sleep in!");
+		
+		}
+		else if (isWeekday) {
+		JOptionPane.showMessageDialog(null,"get up Lazy bones!!");
+		}
+	}
+	
 }
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -9,17 +9,19 @@ public class StringMethods2 {
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
 
+		String method = "whAlmANsaSpicytAcoGuACamOle"; 
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(method.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+System.out.println(method.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+	System.out.println(method.substring(0, 3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(method.charAt(10));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(method.substring(method.length()-3));
 	}
 }
