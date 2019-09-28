aa72f08840335dd15670e5f6d016c7af2905611c
League Student <league@iMac-27.attlocal.net>
2019-09-28 16:56:52 -0700

    adventurequest

--- src/charAt_scanners_optionDialogs/adveturequest.java
+++ src/charAt_scanners_optionDialogs/adveturequest.java
@@ -3,38 +3,42 @@ package charAt_scanners_optionDialogs;
 import javax.swing.JOptionPane;
 
 public class adveturequest {
-public static void main(String[] args) {
-	
-	
-	
-	
+	public static void main(String[] args) {
 
-	
-	 String adventure =JOptionPane.showInputDialog(null, "you wake up on a planet X 785 not knowing were you are a person walks up to you and says he is a human, do you trust him? Y or N");
-	if(adventure.equals("Y")) {
-	JOptionPane.showMessageDialog(null,"he kills you with a laser gun and reveals himself as an alien while you slowly die ");	
-	System.exit(0);
-	}
-	if(adventure.equals("N")) {
-		String m =JOptionPane.showInputDialog(null, "you run away but see an upcoming abandoned base that says power armour test site, Do you go there? Y or N");	 
-		
-		if(m.equals("N")) {
-			JOptionPane.showMessageDialog(null, "you die from an alien beast");
+		String adventure = JOptionPane.showInputDialog(null,
+				"you wake up on a planet X 785 not knowing were you are,a person walks up to you and says he is a human, do you trust him? Y or N");
+		if (adventure.equals("Y")) {
+			JOptionPane.showMessageDialog(null,
+					"he kills you with a laser gun and reveals himself as an alien while you slowly die ");
 			System.exit(0);
-	}
-	
-		
-		if(adventure.equals("Y")) {
-		JOptionPane.showInputDialog(null,"you go into the base and see a full set of doom power armour, take it?  Y or N");	
-		
-		
-	}
-		if(adventure.equals("N")) {
-			JOptionPane.showInputDialog(null,"You do not go into the base but instead wander in the jungle,you die type in you died in the textbox below");	
-		}}
-		if(adventure.equals("Y")) {
+		}
+		if (adventure.equals("N")) {
+			String m = JOptionPane.showInputDialog(null,
+					"you run away but see an upcoming abandoned base that says power armour test site, Do you go there? Y or N");
+
+			if (m.equals("N")) {
+				JOptionPane.showMessageDialog(null, "you die from an alien beast");
+				System.exit(0);
+			}
+
+			if (m.equals("Y")) {
+				String Z = JOptionPane.showInputDialog(null,
+						"you go into the base and see a full set of doom power armour, take it?  Y or N");
+				if (Z.equals("N")) {
+					JOptionPane.showInputDialog(null,
+							"You do not take the power armour but instead wander in the jungle,you die type in you died in the textbox below");
+				}
+				if (Z.equals("Y")) {
+
+					JOptionPane.showMessageDialog(null,
+							 "you take it and put it on luckily it has a radio on ,so you use that to call someone to pick you up, the perosn replies in a language you undertsand you see a drp ship arriving in the sky. You walk toward the ship landing all the humans come and greet you and also strangely look at you because you are covered in armour, you have escaped the planet atmosphere and begun a long journey home.YOOOOOOOOOOOOOOOOOU WIIIIIIIIIIIIN    ");
+					
+				}
 			
-			JOptionPane.showInputDialog(null,"you take it and put it on luckily it has a radio on  , do you try to call someone?  Y or N");	
-}
-}
+			}
+			
+
+		}
+
+	}
 }
