a796d152a0ec47d69fb90ea580a3862e3e620580
League Student <league@iMac-27.attlocal.net>
2019-10-12 17:25:46 -0700

    fibonacci sequence

--- src/fibonaccisequence.java
+++ src/fibonaccisequence.java
@@ -1,37 +1,18 @@
 
 public class fibonaccisequence {
-public static void main(String[] args) {
-	int  u = 3;
-	int l =  1;  
+	public static void main(String[] args) {
+		
+		
 	
+        
+for(int i =4;i>10; i++ ) {
+	System.out.println();
+	int u = 1;
+	int l = 2;
+	int k = u;
+	u = l;
+    l =  k;
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-}
+		}
+	}
 }
