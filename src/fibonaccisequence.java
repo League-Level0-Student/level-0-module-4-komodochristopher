2a5759c92e0050afe9f6d2cb6ef8e66fd4a3e64f
League Student <league@iMac-15.attlocal.net>
2019-10-19 17:27:25 -0700

    fibonacci sequence

--- src/fibonaccisequence.java
+++ src/fibonaccisequence.java
@@ -3,16 +3,18 @@ public class fibonaccisequence {
 	public static void main(String[] args) {
 		
 		
-	
+		long l = 1;
+	long u = 0;
         
-for(int i =4;i>10; i++ ) {
-	System.out.println();
-	int u = 1;
-	int l = 2;
-	int k = u;
+for(int i =0;i<10; i++ ) {
+
+
+	long sum =u + l;	
+	System.out.println(u);
+	long k = u;
 	u = l;
     l =  k;
-	
+	 u =sum;
 		}
 	}
 }
